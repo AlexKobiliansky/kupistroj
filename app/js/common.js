a6c1c6dd2d91054280e0b29cde96f0dd62336cc2
alex <alex_kobilianskiy@bigmir.net>
2020-03-14 13:16:20 +0200

    adaptation complete for sections up to .s-banner

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
 
+    //*** mobile-mnu customization *****//
+    var mmenu = $('#mobile-mnu');
+    var menuLogo = mmenu.data("logo");
+    var $mmenu = mmenu.mmenu({
+        navbars: [{
+            type: 'tabs',
+            content: [ "<div class='logo-wrap'><img src=" + menuLogo + " class=\"img-responsive mm-logo\" alt=\"alt\"/></div>",
+                        "<a href='#mob-menu'>Меню</a>",
+                        "<a href='#mob-catalog'>Продукция</a>"],
+
+        }],
+        "pageScroll": true,
+
+        "navbar": {
+            "title" : "",
+        },
+        "extensions": [
+            "theme-dark",
+            "pagedim-black",
+            "position-front",
+            "fx-listitems-slide",
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-container"
+        },
+    });
+
+    var mmenuBtn = $("#mmenu-btn");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('is-active')
+    });
+
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "is-active" );
+        }, 300);
+    });
+    //***** end mobile-mnu customization *****//
+
+
+
     $('.intro-slider').owlCarousel({
         loop:true,
         nav:false,
